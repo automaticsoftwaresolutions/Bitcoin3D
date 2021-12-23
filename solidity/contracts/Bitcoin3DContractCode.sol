1fcd986926a4b8ae1bace38e508258081f3f8857
Alexander Andrew Wood <alex@automaticsoftwaresolutions.com>
2021-12-23 18:47:25 -0500

    Upgraded Compiled Version To Latest

--- solidity/contracts/Bitcoin3DContractCode.sol
+++ solidity/contracts/Bitcoin3DContractCode.sol
@@ -1,29 +1,30 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.4.26;
+pragma solidity >=0.8.11 <0.9.0;
 
 import "./Token.sol";
 import "./StandardToken.sol";
 
-contract ApproveAndCallFallBack {
-      function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
+abstract contract ApproveAndCallFallBack 
+{
+      function receiveApproval(address from, uint256 tokens, address token, bytes memory data) public virtual;
 }
 
 
 
 //name this contract whatever you'd like
-contract Bitcoin3D is StandardToken {
+contract Bitcoin3D is StandardToken
+{
 
     /* Public variables of the token */
-
     /*
     NOTE:
     The following variables are OPTIONAL vanities. One does not have to include them.
     They allow one to customise the token contract & in no way influences the core functionality.
     Some wallets/interfaces might not even bother to look at this information.
     */
-    uint256 whaleFunding = 30000000000000;
-    uint256 reserved = 100000000000;
+    uint256 _whaleFunding;// = 30000000000000;
+    uint256 _reserved; // = 100000000000;
     string public name;                   //fancy name: eg Simon Bucks
     uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
     string public symbol;                 //An identifier: eg SBX
@@ -34,25 +35,30 @@ contract Bitcoin3D is StandardToken {
 
 //make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of ERC20Token
 
-    constructor(uint256 supply) public
-    {
-        balances[msg.sender] = reserved + whaleFunding;               // Give the creator all initial tokens (100000 for example)
-        totalSupply = supply;                   // Update total supply (100000 for example)
+    constructor(uint256 supply, uint256 reserved, 
+        uint256 whaleFunding, uint numberOfBurns, 
+        uint burnAmount) 
+        StandardToken(supply, numberOfBurns, burnAmount) 
+    {        
+        _reserved = reserved;
+        _whaleFunding = whaleFunding;
+        balances[msg.sender] = _reserved + whaleFunding + (burnAmount * numberOfBurns);               // Give the creator all initial tokens (100000 for example)
         name = "Bitcoin3D";                                   // Set the name for display purposes
         decimals = 18;                            // Amount of decimals for display purposes
         symbol = "Bitcoin3D";                               // Set the symbol for display purposes
     }
 
     /* Approves and then calls the receiving contract */
-    function approveAndCall(address _spender, uint256 _value, bytes memory _extraData) public returns (bool success) {
+    function approveAndCall(address _spender, uint256 _value, 
+        bytes memory _extraData) 
+        public returns (bool success) 
+    {
         allowed[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
-
-
         ApproveAndCallFallBack(_spender).receiveApproval(
             msg.sender,
             _value,
-            this,
+            address(this),
             _extraData
         );
 
--- solidity/contracts/StandardToken.sol
+++ solidity/contracts/StandardToken.sol
@@ -1,41 +1,49 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.4.26;
+pragma solidity >=0.8.11 <0.9.0;
 
 
 import "./Token.sol";
 
-contract StandardToken is Token {
-    uint numberOfBurns = 8;
-    uint currentBurn = 0;
-    uint256 burnAmount = 5000000000000;
-    address burnWallet = 0xdead03D7c5613b7Bd99C8D71dC6571dF55DDe05E;
-    uint[] burnIncrements = [ 0, 0, 0, 0, 0 ];
-    
-
+abstract contract StandardToken is Token 
+{
+    uint _numberOfBurns = 8;
+    uint _currentBurn = 0;
+    uint256 _burnAmount = 5000000000000;
+    address _burnWallet = 0x000000000000000000000000000000000000dEaD;
+    uint[] _burnIncrements = [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
+    uint256 _totalSupply;
+    constructor(uint256 supply, uint numberOfBurns, uint burnAmount)
+    {
+        _totalSupply = supply;
+        _numberOfBurns = numberOfBurns;
+        _burnAmount = burnAmount;
+    }
 
     function ValidateBurn() private
-    {
-        
-        if(numberOfBurns <= currentBurn)
+    {        
+        if(_numberOfBurns <= _currentBurn)
         {
             return;
         }
-        if(block.timestamp < burnIncrements[currentBurn])
+        if(block.timestamp < _burnIncrements[_currentBurn])
         {
             return;
         }
-        ++currentBurn;
-        transfer(burnWallet, burnAmount);
-        totalSupply -= burnAmount;
+        ++_currentBurn;
+        transfer(_burnWallet, _burnAmount);
+        _totalSupply -= _burnAmount;
     }
 
     function MiniBurn() private
     {
-        transfer(burnWallet, 1);
+        transfer(_burnWallet, 1);
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool success) {
+    function transfer(address _to, uint256 _value) override
+        public returns (bool success) 
+    {
+        ValidateBurn();
         //Default assumes totalSupply can't be over max (2^256 - 1).
         //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
         //Replace the if with this one instead.
@@ -51,16 +59,20 @@ contract StandardToken is Token {
                 return false;
             }
             emit Transfer(msg.sender, _to, _value);  
-            if(_to != burnWallet)
+            if(_to != _burnWallet)
             {
                 MiniBurn();
             }          
             return true;
-        } else { return false; }
-        ValidateBurn();
+        } else 
+        { 
+            return false; 
+        }        
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    function transferFrom(address _from, address _to, uint256 _value) override
+        public returns (bool success) 
+    {
         //same as above. Replace this line with the following if you want to protect against wrapping uints.
         //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
         if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
@@ -69,24 +81,39 @@ contract StandardToken is Token {
             allowed[_from][msg.sender] -= _value;
             emit Transfer(_from, _to, _value);            
             return true;
-        } else { return false; }
+        } 
+        else 
+        { 
+            return false; 
+        }
     }
 
-    function balanceOf(address _owner) public view returns (uint256 balance) {
+    function balanceOf(address _owner) override
+        public view returns (uint256 balance) 
+    {
         return balances[_owner];
     }
 
-    function approve(address _spender, uint256 _value) public returns (bool success) {
+    function approve(address _spender, uint256 _value) override
+        public returns (bool success) 
+    {
         allowed[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+    function allowance(address _owner, address _spender) override
+        public view returns (uint256 remaining) 
+    {
       return allowed[_owner][_spender];
     }
-
+   
     mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) allowed;
-    uint256 public totalSupply;
+        
+    function totalSupply() override
+        public view returns (uint256 supply) 
+    {  
+        return _totalSupply;
+    }
 }
