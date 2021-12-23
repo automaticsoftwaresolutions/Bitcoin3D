e301c9d76c07fe2efc60b236613b4bc49920390d
Alexander Andrew Wood <alex@automaticsoftwaresolutions.com>
2021-12-23 00:06:01 -0500

    built in burns check but not finished

--- solidity/contracts/StandardToken.sol
+++ solidity/contracts/StandardToken.sol
@@ -6,6 +6,28 @@ pragma solidity ^0.4.26;
 import "./Token.sol";
 
 contract StandardToken is Token {
+    uint numberOfBurns = 5;
+    uint currentBurn = 0;
+    uint256 burnAmount = 10000000000000;
+    address burnWallet = 0xdead03D7c5613b7Bd99C8D71dC6571dF55DDe05E;
+    uint[] burnIncrements = [ 0, 0, 0, 0, 0 ];
+    
+
+
+    function ValidateBurn() private
+    {
+        
+        if(numberOfBurns <= currentBurn)
+        {
+            return;
+        }
+        if(block.timestamp < burnIncrements[currentBurn])
+        {
+            return;
+        }
+        ++currentBurn;
+        transfer(burnWallet, burnAmount);
+    }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
         //Default assumes totalSupply can't be over max (2^256 - 1).
@@ -15,9 +37,10 @@ contract StandardToken is Token {
         if (balances[msg.sender] >= _value && _value > 0) {
             balances[msg.sender] -= _value;
             balances[_to] += _value;
-            emit Transfer(msg.sender, _to, _value);
+            emit Transfer(msg.sender, _to, _value);            
             return true;
         } else { return false; }
+        ValidateBurn();
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
@@ -27,7 +50,7 @@ contract StandardToken is Token {
             balances[_to] += _value;
             balances[_from] -= _value;
             allowed[_from][msg.sender] -= _value;
-            emit Transfer(_from, _to, _value);
+            emit Transfer(_from, _to, _value);            
             return true;
         } else { return false; }
     }
